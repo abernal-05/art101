ce31851a0b7ed383c65ac5e8160f8f2b92bbb260
abernal-05 <arantxabernal71@gmail.com>
2025-04-21 12:29:13 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Card Memory Game
+// Author: Arantxa Bernal
+// Date: 4/18/25
 
 // Constants
 
@@ -8,8 +8,21 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  // Generate 3x3 grid of shuffled symbols
+    // 3 pairs of matching symbols
+    // Symbols should not be right next to each other
+  
+  // Display symbols for 4 seconds
+    // Covver symbols
+ 
+  // Player picks two covered symbols at a time
+    // Accept user input
+
+  // Validate user input
+    // If symbols match, do not cover
+    // If symbols aren’t a match, cover
+  
+  // Repeat until all symbols are uncovered
 }
 
 function main() {
