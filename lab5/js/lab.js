9d7cc1bb5b0f830c936f1a486d72c8ff663048ed
abernal-05 <arantxabernal71@gmail.com>
2025-04-24 23:41:10 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data types and variables
+// Author: Arantxa Bernal
+// Created: 2025
 
-// Constants
+// Variables
+make = "BMW";
+model = "740 iL";
+color = "white";
+year = "1993";
 
-// Functions
+// Calculations
+age = 2025 - year
+
+//Note, that that is WRITELN with an "L" not WRITEIN
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
