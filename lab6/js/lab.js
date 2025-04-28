01ba942df8f6fe6fa9a8de3a8ec6c693d820638f
abernal-05 <arantxabernal71@gmail.com>
2025-04-28 12:55:18 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 - Arrays and Objects
+// Author: Arantxa Bernal
+// Date: 4/28/25
 
-// Constants
+// Define Variables
 
-// Functions
+myTransport = ["BMW 740 iL", " bus", " train", " bike"];
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// object myMainRide
+
+myMainRide = {
+  make: "BMW",
+  model: "740 iL",
+  color: "White",
+  year: 1993,
+  age: function() {
+    return 2025 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+// output
+document.writeln("Getting around: ", myTransport, "</br>");
+
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
